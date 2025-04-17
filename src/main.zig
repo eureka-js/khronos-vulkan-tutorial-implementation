5c84fb788ead06466a5d7bebd2d4402a0aa96eef
eureka-js <jurstanic@gmail.com>
2025-04-17 20:39:17 +0200

    Do the Setup/Instance chapter of the tutorial

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,5 @@
 const c = @cImport({
-    @cDefine("GLFW_INLCUDE_VULKAN", "");
+    @cDefine("GLFW_INCLUDE_VULKAN", "");
     @cInclude("GLFW/glfw3.h");
 });
 
@@ -10,6 +10,8 @@ const HEIGHT: u32 = 600;
 
 const HelloTriangleApplication = struct {
     window: ?*c.GLFWwindow,
+    instance: c.VkInstance,
+    allocator: std.mem.Allocator,
 
     pub fn run(self: *HelloTriangleApplication) !void {
         try self.initWindow();
@@ -27,7 +29,9 @@ const HelloTriangleApplication = struct {
         self.window = c.glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", null, null);
     }
 
-    fn initVulkan(_: *HelloTriangleApplication) !void {}
+    fn initVulkan(self: *HelloTriangleApplication) !void {
+        try self.createInstance();
+    }
 
     fn mainLoop(self: *HelloTriangleApplication) !void {
         while (c.glfwWindowShouldClose(self.window.?) == 0) {
@@ -36,14 +40,80 @@ const HelloTriangleApplication = struct {
     }
 
     fn cleanup(self: *HelloTriangleApplication) !void {
+        c.vkDestroyInstance(self.instance, null);
+
         c.glfwDestroyWindow(self.window);
 
         c.glfwTerminate();
     }
+
+    fn createInstance(self: *HelloTriangleApplication) !void {
+        const appInfo: c.VkApplicationInfo = .{
+            .sType = c.VK_STRUCTURE_TYPE_APPLICATION_INFO,
+            .pApplicationName = "Hello Triangle",
+            .applicationVersion = c.VK_MAKE_VERSION(1, 0, 0),
+            .pEngineName = "No Engine",
+            .engineVersion = c.VK_MAKE_VERSION(1, 0, 0),
+            .apiVersion = c.VK_API_VERSION_1_0,
+        };
+
+        var glfwExtensionCount: u32 = 0;
+        const glfwExtensions: [*c][*c]const u8 = c.glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        var createInfo: c.VkInstanceCreateInfo = .{
+            .sType = c.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+            .pApplicationInfo = &appInfo,
+            .enabledExtensionCount = glfwExtensionCount,
+            .ppEnabledExtensionNames = glfwExtensions,
+            .enabledLayerCount = 0,
+        };
+
+        if (c.vkCreateInstance(&createInfo, null, &self.instance) != c.VK_SUCCESS) {
+            return error.FailedToCreateInstance;
+        }
+
+        var extensionCount: u32 = 0;
+        _ = c.vkEnumerateInstanceExtensionProperties(null, &extensionCount, null);
+        const extensions = try self.allocator.alloc(c.VkExtensionProperties, extensionCount);
+        defer self.allocator.free(extensions);
+        _ = c.vkEnumerateInstanceExtensionProperties(
+            null,
+            &extensionCount,
+            @ptrCast(extensions.ptr),
+        );
+
+        try self.areExtensionsSupported(glfwExtensions, glfwExtensionCount, extensions);
+    }
+
+    fn areExtensionsSupported(
+        _: *HelloTriangleApplication,
+        glfwExt: [*c][*c]const u8,
+        glfwExtCount: u32,
+        ext: []c.VkExtensionProperties,
+    ) !void {
+        outer: for (glfwExt[0..glfwExtCount]) |name_ptr| {
+            const name = std.mem.span(name_ptr);
+            for (ext) |item| {
+                const ext_name = std.mem.sliceTo(&item.extensionName, 0);
+                if (std.mem.eql(u8, name, ext_name)) {
+                    continue :outer;
+                }
+            }
+
+            return error.ExtensionNotSupported;
+        }
+    }
 };
 
 pub fn main() !void {
-    var app: HelloTriangleApplication = .{ .window = undefined };
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var app: HelloTriangleApplication = .{
+        .window = undefined,
+        .instance = undefined,
+        .allocator = allocator,
+    };
 
     try app.run();
 }
